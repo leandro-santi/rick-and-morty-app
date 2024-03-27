783cc7d0f7c7cd6a5e275beb598ae4176dd026d2
Leandro Santiago <leandro.s4nt1@gmail.com>
2024-03-27 15:36:30 -0400

    Created entities response models

---
+++ app/src/main/java/com/solitudeworks/rickandmortyapp/data/model/Character.kt
@@ -0,0 +1,11 @@
+package com.solitudeworks.rickandmortyapp.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Character(
+    @SerializedName("info")
+    val info: Info,
+    @SerializedName("results")
+    val results: List<Result>
+)

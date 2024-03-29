b5991589fdd60563e401390887c33e36aeff690d
Leandro Santiago <leandro.s4nt1@gmail.com>
2024-03-29 12:31:37 -0400

    Changing kapt to ksp on gradle dependencies

--- app/src/main/java/com/solitudeworks/rickandmortyapp/RickAndMortyApplication.kt
+++ app/src/main/java/com/solitudeworks/rickandmortyapp/RickAndMortyApplication.kt
@@ -1,15 +1,26 @@
 package com.solitudeworks.rickandmortyapp
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
+class RickAndMortyApplication : Application() {
 
-// android:name=".RickAndMortyApplication"
-//@HiltAndroidApp
-//class RickAndMortyApplication : Application() {
-//
-//    override fun onCreate() {
-//        super.onCreate()
-//    }
-//
-//}
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: RickAndMortyApplication? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+    override fun onCreate() {
+        super.onCreate()
+        val context: Context = RickAndMortyApplication.applicationContext()
+    }
+
+}
